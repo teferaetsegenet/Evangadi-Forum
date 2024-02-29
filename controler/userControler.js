7ef2157aecd838966422a5a31f85727fe12b72ac
etgit config --global user.name Your <teferaetsegenet@gmail.com.com>
2024-02-29 00:57:07 -0500

    modified components

--- controler/userControler.js
+++ controler/userControler.js
@@ -1,40 +1,79 @@
 const dbConnection =require('../db/dbConfig')
 const { StatusCodes } = require('http-status-codes');
+const bcrypt = require ("bcrypt");
 
 
 async function register(req, res) {
     const { username, firstname, lastname, email, password } = req.body;
-    if (!username || !firstname || !lastname || !email || !password) {
-        return res.status(StatusCodes.BAD_REQUEST).json({ msg: "please provide all the information" });
+    if (!username || !firstname || !lastname || !email || !password)
+     {
+        return res.status(StatusCodes.BAD_REQUEST).json
+        ({ msg: "please provide all the information" });
     }
     try {
-        const [user] = await dbConnection.query("SELECT username, usersid FROM users WHERE username=? OR email =?", [username, email]);
+        const [user] = await dbConnection.query
+        ("SELECT username, usersid FROM users WHERE username=? OR email =?",
+         [username, email]);
         
         if (user.length > 0) {
-            return res.status(StatusCodes.BAD_REQUEST).json({ msg: "user already exists" });
+            return res.status(StatusCodes.BAD_REQUEST).json
+            ({ msg: "user already exists" });
         }
         
         if (password.length <= 8) {
-            return res.status(StatusCodes.BAD_REQUEST).json({ msg: "password must be at least greater than 8 characters" });
+            return res.status(StatusCodes.BAD_REQUEST).json
+            ({ msg: "password must be greater than 8 characters" });
         }
+// password encription
+const salt = await bcrypt.genSalt(10);
+const hashedPassword = await bcrypt.hash(password, salt);
 
-        await dbConnection.query("INSERT INTO users (username, firstname, lastname, email, password) VALUES (?,?,?,?,?)", [username, firstname, lastname, email, password]);
-    
-        return res.json({ msg: "user created" });
+        await dbConnection.query("INSERT INTO users (username, firstname, lastname, email, password) VALUES (?,?,?,?,?)",
+         [username, firstname, lastname, email, hashedPassword]);
+
+     return res.status(StatusCodes.CREATED).json
+     ({ msg: "user created" });
+
     } catch (err) {
         console.log(err.message);
-        return res.json({ msg: "something went wrong, please try again" });
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json
+        ({ msg: "something went wrong, please try again" });
     }
 }
 
-// .status(StatusCodes.INTERNAL_SERVER_ERROR)
-//       .json({ msg: "something went wrong please try again" });
 
 async function login(req, res){
+    const {email, password} = req.body;
+    if(!email || !password){
+        return res.status(StatusCodes.BAD_REQUEST).json
+        ({msg: "Please provide all the required field" });
+    }
 
-    res.send("You are logged in");
+try {
+    const [user] = await dbConnection.query(
+        "SELECT username, usersid, password FROM users WHERE email = ?",[email]
+    );
+    return re.json({user: user})
+//     if (user.length ==0){
+//         return res.status(StatusCodes.BAD_REQUEST).json
+//         ({msg: "invalid credential"});
+
+//         // compare the password 
+//         const isMatch = await bcrypt.compare(password, user[0].password);
+//         if(!isMatch) {
+//             return res.status(StatusCodes.BAD_REQUEST).json({msg: "invalid credential"})
+//         }
+//     }
+// }
+
+    
+
+    // res.send("You are logged in");
 
 }
+
+
+
 
 async function check(req, res){
 
