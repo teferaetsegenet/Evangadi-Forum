dc0271ee6ecd5daf72d13e7d2a636f1db0e45bfa
etgit config --global user.name Your <teferaetsegenet@gmail.com.com>
2024-02-28 23:10:55 -0500

    modified userControl

--- controler/userControler.js
+++ controler/userControler.js
@@ -1,23 +1,25 @@
 const dbConnection =require('../db/dbConfig')
+const { StatusCodes } = require('http-status-codes');
+
 
 async function register(req, res) {
     const { username, firstname, lastname, email, password } = req.body;
     if (!username || !firstname || !lastname || !email || !password) {
-        return res.json({ msg: "please provide all the information" });
+        return res.status(StatusCodes.BAD_REQUEST).json({ msg: "please provide all the information" });
     }
     try {
         const [user] = await dbConnection.query("SELECT username, usersid FROM users WHERE username=? OR email =?", [username, email]);
         
         if (user.length > 0) {
-            return res.json({ msg: "user already exists" });
+            return res.status(StatusCodes.BAD_REQUEST).json({ msg: "user already exists" });
         }
         
         if (password.length <= 8) {
-            return res.json({ msg: "password must be at least greater than 8 characters" });
+            return res.status(StatusCodes.BAD_REQUEST).json({ msg: "password must be at least greater than 8 characters" });
         }
 
         await dbConnection.query("INSERT INTO users (username, firstname, lastname, email, password) VALUES (?,?,?,?,?)", [username, firstname, lastname, email, password]);
-        
+    
         return res.json({ msg: "user created" });
     } catch (err) {
         console.log(err.message);
