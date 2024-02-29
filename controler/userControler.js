3aefcc1a6baf63d30abe5fb60b6d971a34723df4
etgit config --global user.name Your <teferaetsegenet@gmail.com.com>
2024-02-28 22:07:41 -0500

    Insert users db

--- controler/userControler.js
+++ controler/userControler.js
@@ -1,34 +1,32 @@
 const dbConnection =require('../db/dbConfig')
 
-async function register(req, res){
-const {username, firstname, lastname, email, password} = req.body;
-if(!username || !firstname || !lastname || !email || !password){
-    return res.json({msg:"please provide all the information"})
-}
-try{
-   
-    const [user]= await dbConnection.query("SELECT username, userid, from users where username=? or email =?", 
-    [username,email])
-    // return res.json({user : user})
-
-    // if(user.length>0){
-    //     return res.json({msg:"user allready exist"})
-    // }
-    if (password.length<=8){
-        return res.json({msg:"password must be atleast 8 character"})
+async function register(req, res) {
+    const { username, firstname, lastname, email, password } = req.body;
+    if (!username || !firstname || !lastname || !email || !password) {
+        return res.json({ msg: "please provide all the information" });
     }
+    try {
+        const [user] = await dbConnection.query("SELECT username, usersid FROM users WHERE username=? OR email =?", [username, email]);
+        
+        if (user.length > 0) {
+            return res.json({ msg: "user already exists" });
+        }
+        
+        if (password.length <= 8) {
+            return res.json({ msg: "password must be at least greater than 8 characters" });
+        }
 
-
-await dbConnection.query("INSERT INTO users (username, firstname, lastname, email, password) VALUES (?,?,?,?,?)", [username, firstname, lastname, email, password])
-return res.json({msg: "user created"})
-
-
-}catch(err){
-    console.log(err.message)
-    return res.json({msg:"someting went wrong please try again"})
+        await dbConnection.query("INSERT INTO users (username, firstname, lastname, email, password) VALUES (?,?,?,?,?)", [username, firstname, lastname, email, password]);
+        
+        return res.json({ msg: "user created" });
+    } catch (err) {
+        console.log(err.message);
+        return res.json({ msg: "something went wrong, please try again" });
+    }
 }
 
-}
+// .status(StatusCodes.INTERNAL_SERVER_ERROR)
+//       .json({ msg: "something went wrong please try again" });
 
 async function login(req, res){
 
